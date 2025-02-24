f490c24597d23e528868e9e449aa2b04df9bcf27
Oh-jaesik <ojs72272@gmail.com>
2025-02-24 11:09:37 +0900

    first

---
+++ day11.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# a = np.array([1,3,-9,2])
+# print(a, a.ndim)
+#
+# b= np.array([[1,2,3,4,],[5,6,7,8],[9,10,11,12]])
+# print(b, b.ndim)
+
+ones = np.ones((3,4))
+print(ones)
+
+zeros = np.zeros((3,4), dtype = np.int16)
+print(zeros)
